f61dbe19326187e9e3cc138532ea959eea86a106
Creator-Lynx <1_linx_1@mail.ru>
2022-07-22 16:04:49 +0400

    Complete the clocks guide.
    maked a working clocks with materials, correct objects and scripts.

---
+++ Made a Clock/Assets/Scripts/ClockBehavior.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class ClockBehavior : MonoBehaviour
+{
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+    const float hoursToDegrees = 30;
+    const float minutesToDegrees = 6;
+
+    void Update()
+    {
+        var now = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float)now.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)now.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)now.TotalSeconds);
+
+    }
+
+}
