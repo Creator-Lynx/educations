7d5d389c3ed2b2e3f30cba19d9efff9d0269cc90
Creator-Lynx <1_linx_1@mail.ru>
2022-07-23 13:19:17 +0400

    finished made a clock project
    comleted the scene with clock and make correctly working accelerometer
    for andriod
    changed the materials and lights

--- Made a Clock/Assets/Scripts/CameraRotatingByAccelerometer.cs
+++ Made a Clock/Assets/Scripts/CameraRotatingByAccelerometer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraRotatingByAccelerometer : MonoBehaviour
 {
     [SerializeField]
-    float lerpSpeed = 0.2f, accelerationFactor = 2;
+    float lerpSpeed = 1f, accelerationFactor = 2.5f;
 
     [SerializeField]
     Transform target;
@@ -15,15 +15,17 @@ public class CameraRotatingByAccelerometer : MonoBehaviour
     }
     Vector3 acceleration;
     Vector3 shift;
+    Vector3 delta = Vector3.zero;
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(target);
         var acc = Input.acceleration * accelerationFactor;
         acceleration = new Vector3(-acc.x, acc.z, 0);
-        var moving = Vector3.Slerp(transform.position, shift + acceleration, lerpSpeed);
-        transform.position = new Vector3(Mathf.Clamp(moving.x, shift.x - 8, shift.x + 8),
+        var moving = Vector3.SmoothDamp(transform.position, shift + acceleration, ref delta, lerpSpeed);
+        transform.position = moving;
+        /*transform.position = new Vector3(Mathf.Clamp(moving.x, shift.x - 8, shift.x + 8),
                                         Mathf.Clamp(moving.y, shift.y - 8, shift.y + 8),
-                                        Mathf.Clamp(moving.z, shift.z - 3, shift.z + 3));
+                                        Mathf.Clamp(moving.z, shift.z - 3, shift.z + 3));*/
     }
 }
